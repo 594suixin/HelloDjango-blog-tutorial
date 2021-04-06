93fd5c386da8837c1af097e41d9d3648d6569f4c
suixin <2409141617@qq.com>
2021-04-06 21:51:10 +0800

    add all file

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,42 @@
+from django import template
+from ..models import Post, Category, Tag
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/inclusions/_recent_posts.html', takes_context=True)
+def show_recent_posts(context, num=5):
+    return {
+        'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_archives.html', takes_context=True)
+def show_archives(context):
+    return {
+        'date_list': Post.objects.dates('created_time', 'month', order='DESC'),
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
+def show_categories(context):
+    return {
+        'category_list': Category.objects.all(),
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
+def show_tags(context):
+    return {
+        'tag_list': Tag.objects.all(),
+    }
+
+
+@register.inclusion_tag('comments/inclusions/_list.html', takes_context=True)
+def show_comments(context, post):
+    comment_list = post.comment_set.all().order_by('-created_time')
+    comment_count = comment_list.count()
+    return {
+        'comment_count': comment_count,
+        'comment_list': comment_list,
+    }
